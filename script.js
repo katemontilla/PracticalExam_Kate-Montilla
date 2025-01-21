4fbd796ba34520709192547f35061d6d80908cc9
katemontilla <keithtipdas@gmail.com>
2025-01-21 19:01:33 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button by its ID
+const alertButton = document.getElementById('alertButton');
+
+// Add a click event listener
+alertButton.addEventListener('click', function () {
+    alert('Hello, welcome to my webpage!');
+});
